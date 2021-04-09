e91940f474c9014f393d08bd97de94d229c102bb
Isabela Carrara <isabelapcarrara@hotmail.com>
2021-04-09 19:28:47 -0300

    Fix carousel

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -2,13 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>https://portfolioisabela.000webhostapp.com</homePageLink>
+		<homePageLink>http://localhost/Portfolio</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://portfolioisabela.000webhostapp.com/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://portfolioisabela.000webhostapp.com/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://portfolioisabela.000webhostapp.com/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://portfolioisabela.000webhostapp.com/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://portfolioisabela.000webhostapp.com/index.php?rest_route=/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="http://localhost/Portfolio/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="http://localhost/Portfolio/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://localhost/Portfolio/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="http://localhost/Portfolio/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="http://localhost/Portfolio/index.php?rest_route=/" />
 			</apis>
 	</service>
 </rsd>
